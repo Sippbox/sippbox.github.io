75aa876783c6fc8a0bbe6be9c16954fb5c3ce442
Sippbox <92064053+Sippbox@users.noreply.github.com>
2023-08-13 22:23:32 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const typingSpeed = 100; // milliseconds per character
+const typingElement = document.getElementById("type");
+const textToType = typingElement.textContent;
+
+typingElement.innerText = "";
+
+let currentIndex = 0;
+
+function typeNextCharacter() {
+  if (currentIndex < textToType.length) {
+    typingElement.textContent += textToType[currentIndex];
+    currentIndex++;
+    setTimeout(typeNextCharacter, typingSpeed);
+  }
+}
+
+// Start the typing effect
+typeNextCharacter();
